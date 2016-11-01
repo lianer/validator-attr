6d4cc6ea0be7f0c7353622c2f36c3d4deedd4733
Lou Yunlong <lianer@live.cn>
2016-11-01 17:13:57 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # validator-attr.js
 
 ![](http://shields.hust.cc/build-passing-green.svg)
-![](http://shields.hust.cc/version-1.0.2-blue.svg)
+![](http://shields.hust.cc/version-1.0.3-blue.svg)
 ![](http://shields.hust.cc/dependence-jQuery-blue.svg)
 
 实现element attribute表单验证，依赖 jQuery or Zepto
@@ -25,6 +25,17 @@ new Validator("#form", {
 ```
 
 ## Changelog
-* 2016.11.01 移除默认同步提交表单的逻辑，必须通过success回调
-* 2016.10.09 增加验证方法，增加success，error配置项
-* 2016.04.25 published
+
+**2016.11.01 v1.0.3**
+
+- 增加range验证方法，支持验证radio/checkbox/select-multiple选中的个数
+- 修改默认同步提交表单的逻辑，现在必须通过success回调函数处理
+- 修改required针对radio/checkbox的处理逻辑，现在判断同名的radio/checkbox是否有被选中
+
+**2016.10.09 v1.0.2**
+
+- 增加验证方法，增加success，error配置项
+
+**2016.04.25 v1.0.0**
+
+- published
--- validator-attr.js
+++ validator-attr.js
@@ -2,7 +2,7 @@
  * validator-attr.js 通用表单验证类
  * 
  * @author lianer
- * @version 1.0.2
+ * @version 1.0.3
  * @description 实现element attribute表单验证，依赖 jQuery or Zepto，ie8+
  *
  * @example
@@ -15,7 +15,7 @@
  *   success: function (form) {
  * 
  *   },
- *   error: function (msg) {
+ *   error: function (msg, elem) {
  * 
  *   }
  * })
@@ -58,7 +58,7 @@
 					v.options.success && v.options.success(v.form[0]);
 				}
 				else{
-					v.options.error && v.options.error(result.message);
+					v.options.error && v.options.error(result.message, result.elem);
 				}
 			}catch(e){
 				console.log(e);
@@ -74,30 +74,31 @@
 		var elems = v.form[0].elements; // 表单的元素
 		var pass = true;
 		var message = "";
+		var i, elem, methodName, upperCase, ruleName, msgName, elemRule, required, valid;
 
 		// 遍历表单元素，遍历自定义验证方法，进行冒泡
-		loop: for (var i = 0; i < elems.length; i++) {
-			var elem = $(elems[i]);
+		loop: for (i = 0; i < elems.length; i++) {
+			elem = $(elems[i]);
 
-			for (var methodName in methods){
+			for (methodName in methods){
 				// 首字母大写
-				var upperCase = methodName.replace(/\w/, function ($0) {
+				upperCase = methodName.replace(/\w/, function ($0) {
 					return $0.toUpperCase();
 				});
 
-				var ruleName = "rule" + upperCase;
-				var msgName = "msg" + upperCase;
-				var elemRule = elem.data(ruleName);
+				ruleName = "rule" + upperCase;
+				msgName = "msg" + upperCase;
+				elemRule = elem.data(ruleName);
 
 				// 非必填并且值为空，跳过这个元素的所有验证
-				var required = elem.data("ruleRequired") == 1 || elem.data("ruleRequired") == true;
+				required = elem.data("ruleRequired") == 1 || elem.data("ruleRequired") == true;
 				if(!required && elem.val() === "") {
 					continue loop;
 				}
 
 				// 如果存在验证规则
 				if(elemRule !== void 0){
-					var valid = methods[methodName].fn(elem.val(), elem[0], elemRule);
+					valid = methods[methodName].fn(elem.val(), elemRule, elem[0], v.form[0]);
 					if(!valid){
 						message = elem.data(msgName);
 						if(!message){
@@ -112,11 +113,12 @@
 
 		return {
 			pass: pass,
-			message: message
+			message: message,
+			elem: elem[0]
 		};
 	};
 
-	Validator.addMethod("required", function (value, elem, param) {
+	Validator.addMethod("required", function (value, param, elem, form) {
 		if(!param){
 			return value === "";
 		}
@@ -135,31 +137,45 @@
 			return $(elem).val().length > 0;
 		}
 		else if(elem.type === "radio"){
-			return elem.checked;
+			return $(form).find("[name='" + elem.name + "']:checked").length;
 		}
 		else if(elem.type === "checkbox"){
-			return elem.checked;
+			return $(form).find("[name='" + elem.name + "']:checked").length;
 		}
 		else if(elem.type === "textarea"){
 			return !!value.length;
 		}
 	}, "请填写必填信息");
 
-	Validator.addMethod("mobile", function (value, elem, param) {
+	Validator.addMethod("mobile", function (value, param, elem, form) {
 		return /^1[0-9]{10}$/.test(value);
 	}, "请输入正确的手机号");
 
-	Validator.addMethod("idcard", function (value, elem, param) {
+	Validator.addMethod("idcard", function (value, param, elem, form) {
 		return /^\d{17}[0-9x]$/i.test(value);
 	}, "请输入正确的身份证号");
 
-	Validator.addMethod("rangelength", function (value, elem, param) {
+	// 字符长度范围，适用于text,hidden,select-one,textarea
+	Validator.addMethod("rangelength", function (value, param, elem, form) {
 		var length = value.length;
 		var range = param.split(",");
 		return length >= $.trim(range[0]) && length <= $.trim(range[1]);
 	}, "字数不够或超出");
 
-	Validator.addMethod("pattern", function (value, elem, param) {
+	// 选择个数范围，适用于select-multiple,checkbox
+	Validator.addMethod("range", function (value, param, elem, form) {
+		var count;
+		var range = param.split(",");
+		if(elem.type === "select-multiple"){
+			count = $(elem).find("option:checked").length
+		}
+		else{
+			count = $(form).find("[name='" + elem.name + "']:checked").length
+		}
+		return count >= $.trim(range[0]) && count <= $.trim(range[1]);
+	}, "选择的个数不正确");
+
+	Validator.addMethod("pattern", function (value, param, elem, form) {
 		var matchPattern = /^\/([^\/]+)\/(\w*)$/;
 		var matched = false;
 		param.replace(matchPattern, function ($0, pattern, attributes) {
